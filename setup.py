35bf7d794c5d0b6ddd9563328b70c4ab1ab5344f
Louis Estève <louis.esteve@universite-paris-saclay.fr>
2024-03-20 18:05:56 +0100

    FEAT: add setup.py at root directory

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+
+setup(
+    name="diversutils",
+    author="Louis Estève",
+    author_email="louis.esteve@universite-paris-saclay.fr",
+    description="DiversUtils - Functions to measure diversity",
+    version="0.1.0",
+    ext_modules = [
+        Extension(
+            "diversutils",
+            sources=["diversutils/src/diversutilsmodule.c"],
+            extra_compile_args=["-Wall", "-Wextra", "-pedantic", "-Idiversutils/src/include", "-std=c99", "-lm"]
+        )
+    ]
+)
+
