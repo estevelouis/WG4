793f2c4d1549722ff2825a2e9cf508ee78f45e4b
Louis Estève <louis.esteve@universite-paris-saclay.fr>
2024-07-08 17:11:17 +0200

    INSTALL: update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description="DiversUtils - Functions to measure diversity",
     version="0.1.4",
     packages=["diversutils"],
-    package_dir={"diversutils": "diversutils/."},
+    package_dir={"diversutils": "diversutils/diversutils"},
     ext_modules = [
         Extension(
             name="_diversutils",
@@ -19,7 +19,7 @@ setup(
             define_macros=[("ENABLE_AVX256", "0"), ("ENABLE_AVX512", "0")],
             library_dirs=[f"{os.environ['HOME']}/.local/lib/diversutils"],
             libraries=["m", "rt", "diversutils", "udpipe"],
-            extra_compile_args=["-g3", "-Wall", "-Wextra", "-pedantic", "-Werror", "-std=c99", "-pthread"]
+            extra_compile_args=["-g3", "-Wall", "-Wextra", "-pedantic", "-Werror", "-std=c99", "-pthread", "-fstack-protector-all"]
         )
     ]
 )
